e972da2d04f3e15a9a845984096b94028e2216c9
Your Name <you@example.com>
2021-10-22 19:31:45 -0300

    design board partially done

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,21 +5,11 @@ import Square from './Square';
     
     return (
         <div className="board">
-            <div className="row">
-                <Square value={board[0]} onClick={(() =>onClick(0))}/>
-                <Square value={board[1]} onClick={(() =>onClick(1))}/>
-                <Square value={board[2]} onClick={(() =>onClick(2))}/>
-            </div>
-            <div className="row">
-                <Square value={board[3]} onClick={(() =>onClick(3))}/>
-                <Square value={board[4]} onClick={(() =>onClick(4))}/>
-                <Square value={board[5]} onClick={(() =>onClick(5))}/>
-            </div>
-            <div className="row">
-                <Square value={board[6]} onClick={(() =>onClick(6))}/>
-                <Square value={board[7]} onClick={(() =>onClick(7))}/>
-                <Square value={board[8]} onClick={(() =>onClick(8))}/>
-            </div>
+            {
+                board.map((square, i) => {
+                    return <Square key={i} value={board[i]} onClick={(() =>onClick(i))}/>
+                })
+            }
         </div>
     )
 };
--- src/components/Square.js
+++ src/components/Square.js
@@ -3,12 +3,33 @@ import Player from './Player';
 
  const Square = ({ value, onClick }) => {
     return (
-        <>
-            <button style={{width: '50px', height: '50px'}} onClick={() => onClick(value)}>
+        <div style={squareStyle.box}>
+            <button style={squareStyle.button} onClick={() => onClick(value)}>
                 { value !== "" ? value ==='Player One' ? <Player player={'Player One'}/> : <Player player={'Player Two'}/> : ""}
             </button>
-        </>
+        </div>
     )
 };
+
+const squareStyle = {
+    box: {
+        width: "30%",
+        height: "100px",
+        background: "gray",
+        float: "left",
+        margin: "5px",
+        color: "white",
+        verticalAlign: "middle",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    button: {
+        background:  "gray",
+        width: '100px',
+        height: '100px'
+    }
+    
+  };
 
 export default Square;
